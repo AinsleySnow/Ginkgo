476f3d95a013c71e0765f8f8bbdddd736c2cda3e
AinsleySnow <AinsleySnow@outlook.com>
2022-07-24 11:37:54 +0800

    add enums to be used in symbol tables

---
+++ front/EnumsforEntry.h
@@ -0,0 +1,23 @@
+#ifndef _ENUMS_FOR_ENTRY_H_
+#define _ENUMS_FOR_ENTRY_H_
+
+enum class typespec // type specifier
+{
+    int8,
+    int16,
+    int32,
+    int64,
+    float32,
+    float64,
+    _void, // void
+};
+
+enum class typequal // type qualifier
+{
+    _const,
+    _restrict,
+    _volatile,
+    _atomic
+};
+
+#endif // _ENUMS_FOR_ENTRY_H_
