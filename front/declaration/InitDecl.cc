3849fdb656e87843efe9c32580f7df8305c9fca4
AinsleySnow <AinsleySnow@outlook.com>
2022-08-06 21:24:18 +0800

    add copy ctor; otherwise parser.cc won't compile

--- front/declaration/InitDecl.cc
+++ front/declaration/InitDecl.cc
@@ -1,12 +1,24 @@
 #include "InitDecl.h"
 
-InitDecl::InitDecl(std::unique_ptr<Declarator>& d)
+InitDecl::InitDecl(const InitDecl& id)
+{
+    InitDecl& nonconst = const_cast<InitDecl&>(id);
+    initializer = std::move(nonconst.initializer);
+    declarator = std::move(nonconst.declarator);
+}
+
+InitDecl::InitDecl(std::unique_ptr<Declarator>&& d)
 {
     declarator = std::move(d);
 }
 
-InitDecl::InitDecl(std::unique_ptr<Declarator>& d, std::unique_ptr<Init>& i)
+InitDecl::InitDecl(std::unique_ptr<Declarator>&& d, std::unique_ptr<Init>&& i)
 {
     initializer = std::move(i);
     declarator = std::move(d);
+}
+
+const std::string& InitDecl::GetName() const
+{
+    return declarator->directdecl->identifier;
 }
--- front/declaration/InitDecl.h
+++ front/declaration/InitDecl.h
@@ -12,10 +12,11 @@ public:
     std::unique_ptr<Declarator> declarator{ nullptr };
     std::unique_ptr<Init> initializer{ nullptr };
 
-    InitDecl(std::unique_ptr<Declarator>&);
-    InitDecl(std::unique_ptr<Declarator>&, std::unique_ptr<Init>&);
+    InitDecl(const InitDecl&);
+    InitDecl(std::unique_ptr<Declarator>&&);
+    InitDecl(std::unique_ptr<Declarator>&&, std::unique_ptr<Init>&&);
 
-    const std::string& GetName();
+    const std::string& GetName() const;
 };
 
 #endif // _INITDECL_H_
