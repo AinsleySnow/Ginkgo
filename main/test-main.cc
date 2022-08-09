b899a682c7d8b040c0798f63b624e8c4b28fc60d
AinsleySnow <AinsleySnow@outlook.com>
2022-08-09 13:38:45 +0800

    a complete testing framework

--- main/test-main.cc
+++ main/test-main.cc
@@ -1,8 +1,8 @@
 #include <cassert>
 #include <cstdio>
 #include <cstring>
-#include <array>
 #include <functional>
+#include <map>
 #include <memory>
 #include <string>
 
@@ -16,54 +16,109 @@ extern int yydebug;
 #endif
 
 extern FILE* yyin;
+using map = std::map<std::string, std::function<bool(const SymbolTable&)>>;
+static const std::string path{ "main/test-cases/" };
+static const std::string extend{ ".c" };
 
-using pair = std::pair<std::string, std::function<bool(void)>>;
-using pair_array = std::array<pair, 1>;
 
-bool test_speccomb();
+bool test_speccomb(const SymbolTable&);
 
-static const pair_array files{
-    pair("main/test-cases/spec-combination.c", test_speccomb)
+static const map files{
+    {"spec-combination", test_speccomb}
 };
 
 
-bool test_speccomb()
+bool test_speccomb(const SymbolTable& st)
 {
-    printf("spec-combination.c: ");
-    yyin = fopen(files[0].first.c_str(), "r");
-    assert(yyin);
-
-    SymbolTable globalSymbols{};
-    yyparse(globalSymbols);
-    globalSymbols.PrintSymbols();
-
+    st.PrintSymbols();
     return true;
 }
 
 
-int main(void)
+bool run(const std::string& name,
+    const std::function<bool(const SymbolTable&)>& func)
 {
-#ifdef YYDEBUG
-    yydebug = 1;
-#endif
-
-    for (pair func : files)
+    try
     {
-        try
+        yyin = fopen((path + name + extend).c_str(), "r");
+        assert(yyin != nullptr);
+        SymbolTable globalSymbols{};
+        yyparse(globalSymbols);
+
+        printf("%s: ", name.c_str());
+        if (func(globalSymbols))
         {
-            if (func.second())
-                printf("ok\n");
-            else
-                printf("error\n");
+           printf("ok\n");
+           return true;
         }
-        catch (const std::exception& e)
+        else
         {
-            printf("exception thrown\n");
-        }
-        catch (int e)
-        {
-            printf("%d\n", e);
+            printf("error\n");
+            return false;
         }
     }
+    catch (const std::exception& e)
+    {
+        printf("exception thrown\n");
+        return false;
+    }
+    catch (int e)
+    {
+        printf("An exception has been thrown by the front end: %d\n", e);
+        return false;
+    }
+}
+
+
+int main(int argc, char** args)
+{
+    std::string justone{};
+    if (argc > 3 && (argc != 5 && argc != 4))
+    {
+        printf("usage: test [-t] [<case-name>] [-o <file>]\n");
+        return argc;
+    }
+    for (int i = 1; i < argc; ++i)
+    {
+        if (strcmp("-t", args[i]) == 0)
+        {
+            #ifdef YYDEBUG
+            yydebug = 1;
+            #else
+            printf("YYDEBUG not defined. Ignoring \"-t\".\n");
+            #endif
+        }
+        else if (strcmp("-o", args[i]) == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                printf("usage: test [-t] [<case-name>] [-o <file>]\n");
+                return -1;
+            }
+            freopen(args[i + 1], "w+", stderr);
+            i += 1;
+        }
+        else
+            justone = args[i];
+    }
+
+    if (!justone.empty())
+    {
+        run(justone, files.at(justone));
+        return 0;
+    }
+
+    int pass = 0, fail = 0;
+    for (auto pair : files)
+    {
+        if (run(pair.first, pair.second))
+            ++pass;
+        else
+            ++fail;
+    }
+    if (fail)
+        printf("Tested %d case(s) -- %d passed, %d failed.\n", pass + fail, pass, fail);
+    else
+        printf("All cases passed!\n");
     return 0;
 }
