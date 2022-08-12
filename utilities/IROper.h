1ec1657aaa758ba03265072393fa3cadb9ec11e5
AinsleySnow <AinsleySnow@outlook.com>
2022-08-12 16:20:59 +0800

    add more operations in IROper

--- utilities/IROper.h
+++ utilities/IROper.h
@@ -4,11 +4,11 @@
 enum class IROper
 {
     assign,
-    plus,
-    minus,
-    multiple,
-    divide,
-    mod
+    plus, minus,
+    multiple, divide, mod,
+    inc, dec,
+    getaddr, dereference, positive, negative, bitwisenot, logicalnot,
+    getsize, getalign
 };
 
 #endif // _IROPER_H_
