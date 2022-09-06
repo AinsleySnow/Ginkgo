ffb560a4ad21ef6c788180b6f6f7c00332589798
AinsleySnow <AinsleySnow@outlook.com>
2022-09-06 18:08:53 +0800

    node initalizer

--- front/declaration/Init.h
+++ front/declaration/Init.h
@@ -2,13 +2,18 @@
 #define _INIT_H_
 
 #include "../Node.h"
+#include "../expression/AssignExpr.h"
 
 class Init : public Node
 {
-private:
-    /* data */
 public:
-    Init(/* args */);
+    std::unique_ptr<AssignExpr> assignExpr;
+    std::unique_ptr<InitList> initList;
+
+    Init(std::unique_ptr<InitList>&& il) :
+        initList(std::move(il)) {};
+    Init(std::unique_ptr<AssignExpr>&& ae) :
+        assignExpr(std::move(ae)) {};
 };
 
 #endif // _INIT_H_
