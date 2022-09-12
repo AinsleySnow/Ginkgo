9fdc226cd6c69b0f8733233a233ca7312401fbde
AinsleySnow <AinsleySnow@outlook.com>
2022-09-12 22:01:10 +0800

    declare TypeSpec as 'class' and move it to a new file

--- utilities/EnumsforEntry.h
+++ utilities/EnumsforEntry.h
@@ -1,23 +1,6 @@
 #ifndef _ENUMS_FOR_ENTRY_H_
 #define _ENUMS_FOR_ENTRY_H_
 
-enum class TypeSpec // type specifier
-{
-    int8,
-    int16,
-    int32,
-    int64,
-    uint8,
-    uint16,
-    uint32,
-    uint64,
-    float32,
-    float64,
-    _void, // void
-    _bool,
-    undef
-};
-
 enum class TypeQual // type qualifier
 {
     _const,
---
+++ utilities/TypeSpec.h
@@ -0,0 +1,65 @@
+#ifndef _TYPESPEC_H_
+#define _TYPESPEC_H_
+
+#include <string>
+#include <variant>
+
+class TypeSpec
+{
+private:
+    std::variant<int, std::string> type;
+
+public:
+    TypeSpec(int t) : type(t) {}
+    explicit TypeSpec(const std::string& s) : type(s) {}
+
+    static const int _bool = -1, 
+    int8 = 0, uint8 = 1, int16 = 2,
+    uint16 = 3, int32 = 4, uint32 = 5,
+    int64 = 6, uint64 = 7, float32 = 8, 
+    float64 = 9, _void = -2, undef = -3;
+
+    bool operator<(const TypeSpec ts) const
+    {
+        if (type.index() == 0 && ts.type.index() == 0)
+            return std::get<0>(type) < std::get<0>(ts.type);
+        throw 1002;
+    }
+
+    bool operator<(int ts) const
+    {
+        if (type.index() == 0)
+            return std::get<0>(type) < ts;
+        throw 1002;
+    }
+
+    bool operator>(const TypeSpec ts) const
+    {
+        if (type.index() == 0 && ts.type.index() == 0)
+            return std::get<0>(type) > std::get<0>(ts.type);
+        throw 1002;
+    }
+
+    bool operator>(int ts) const
+    {
+        if (type.index() == 0)
+            return std::get<0>(type) > ts;
+        throw 1002;
+    }
+    
+    bool operator==(int ts) const
+    {
+        if (type.index() == 0)
+            return ts == std::get<0>(type);
+        return false;
+    }
+
+    bool operator==(const std::string& name) const
+    {
+        if (type.index() == 1)
+            return name == std::get<1>(type);
+        return false;
+    }
+};
+
+#endif // _TYPESPEC_H_
