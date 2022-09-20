344e6eb736f141d791f59b1766e92c97641f44a3
AinsleySnow <AinsleySnow@outlook.com>
2022-09-20 14:53:58 +0800

    return IR directly when there's only one expr

--- front/expression/Expression.cc
+++ front/expression/Expression.cc
@@ -3,6 +3,8 @@
 IR Expression::Generate(SymbolTable& st) const
 {
     IR lotsOfExpr{};
+    if (exprs.size() == 1)
+        return exprs.begin()->Generate(st);
     for (const AssignExpr& ae : exprs)
     {
         IR temp = ae.Generate(st);
