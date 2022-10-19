7e7dd2acacbb07f50ee13cf808c54dcdc07e0a19
AinsleySnow <AinsleySnow@outlook.com>
2022-10-19 23:03:50 +0800

    use pointer instead of reference; use ErrorId; implemented DoCalc

--- src/ast/Constant.cc
+++ src/ast/Constant.cc
@@ -2,54 +2,70 @@
 #include "ArithmType.h"
 #include "messages/Error.h"
 
-bool Constant::DoCalc(Tag op, const Constant& num, Constant& output)
+
+bool Constant::DoCalc(Tag op, const Constant* num, Constant& output)
 {
     switch (op)
     {
-    case Tag::dec: 
+    case Tag::dec: case Tag::inc: case Tag::arrow:
+    case Tag::_and: case Tag::dot:
+        Error(ErrorId::needlval); return false;
+    case Tag::exclamation:
+        if (num->GetInt()) output.val_.intgr_ = 1;
+        else output.val_.intgr_ = 0;
+        return true;
+    case Tag::tilde:
+        if (num->GetType()->IsFloat())
+        {
+            Error(ErrorId::operatormisuse);
+            return false;
+        } 
+        output.val_.intgr_ = ~output.val_.intgr_;
+        return true;
     }
+
+    return false;
 }
 
-bool Constant::DoCalc(Tag op, const Constant& left, const Constant& right, Constant& output)
+bool Constant::DoCalc(Tag op, const Constant* left, const Constant* right, Constant& output)
 {
-#define both_float (left.GetType())->IsFloat() && (right.GetType())->IsFloat()
-#define both_int (left.GetType())->IsInteger() && (right.GetType())->IsInteger()
-#define int_float (left.GetType())->IsInteger() && (right.GetType())->IsFloat()
-#define float_int (left.GetType())->IsFloat() && (right.GetType())->IsInteger()
+#define both_float (left->GetType())->IsFloat() && (right->GetType())->IsFloat()
+#define both_int (left->GetType())->IsInteger() && (right->GetType())->IsInteger()
+#define int_float (left->GetType())->IsInteger() && (right->GetType())->IsFloat()
+#define float_int (left->GetType())->IsFloat() && (right->GetType())->IsInteger()
 #define handle_int(sym)                                         \
     if (both_int)                                               \
     {                                                           \
-        output = Constant(left.GetInt() sym right.GetInt());    \
+        output = Constant(left->GetInt() sym right->GetInt());  \
         return true;                                            \
     }                                                           \
     else                                                        \
     {                                                           \
-        Error(0);                                               \
+        Error(ErrorId::operatormisuse);                         \
         return false;                                           \
     }
 
-#define handle_calc(sym)                                        \
-    if (both_float)                                             \
-    {                                                           \
-        output = Constant(left.GetFloat() sym right.GetFloat());\
-        return true;                                            \
-    }                                                           \
-    else if (both_int)                                          \
-    {                                                           \
-        output = Constant(left.GetInt() sym right.GetInt());    \
-        return true;                                            \
-    }                                                           \
-    else if (int_float)                                         \
-    {                                                           \
-        output = Constant(left.GetInt() sym right.GetFloat());  \
-        return true;                                            \
-    }                                                           \
-    else if (float_int)                                         \
-    {                                                           \
-        output = Constant(left.GetFloat() sym right.GetInt());  \
-        return true;                                            \
+#define handle_calc(sym)                                          \
+    if (both_float)                                               \
+    {                                                             \
+        output = Constant(left->GetFloat() sym right->GetFloat());\
+        return true;                                              \
+    }                                                             \
+    else if (both_int)                                            \
+    {                                                             \
+        output = Constant(left->GetInt() sym right->GetInt());    \
+        return true;                                              \
+    }                                                             \
+    else if (int_float)                                           \
+    {                                                             \
+        output = Constant(left->GetInt() sym right->GetFloat());  \
+        return true;                                              \
+    }                                                             \
+    else if (float_int)                                           \
+    {                                                             \
+        output = Constant(left->GetFloat() sym right->GetInt());  \
+        return true;                                              \
     }
-
 
     switch (op)
     {
@@ -72,7 +88,6 @@ bool Constant::DoCalc(Tag op, const Constant& left, const Constant& right, Const
     case Tag::notequal: handle_calc(!=); break;
     }
 
-    Error(1);
     return false;
 
 #undef both_int
--- src/ast/Constant.h
+++ src/ast/Constant.h
@@ -18,19 +18,20 @@ private:
         double flt_;
     } val_;
 
+
 public:
-    Constant() : Expr(Which::constant) {}
-    explicit Constant(uint64_t u) : Expr(Which::constant)
+    Constant() {}
+    explicit Constant(uint64_t u)
     {
         val_.intgr_ = u;
         type_ = std::make_shared<ArithmType>();
     }
-    explicit Constant(double d) : Expr(Which::constant)
+    explicit Constant(double d)
     {
         val_.flt_ = d;
         type_ = std::make_shared<ArithmType>();
     }
-    explicit Constant(bool b) : Expr(Which::constant)
+    explicit Constant(bool b)
     {
         val_.intgr_ = b ? 1 : 0;
         type_ = std::make_shared<ArithmType>();
@@ -40,10 +41,11 @@ public:
     uint64_t GetInt() const { return val_.intgr_; }
     double GetFloat() const { return val_.flt_; }
 
-    void Accept(Visitor* v) { v->VisitConstant(this); }
+    void Accept(Visitor* v) override { v->VisitConstant(this); }
+    Constant* ToConstant() override { return this; }
 
-    static bool DoCalc(Tag, const Constant&, Constant&);
-    static bool DoCalc(Tag, const Constant&, const Constant&, Constant&);
+    static bool DoCalc(Tag, const Constant*, Constant&);
+    static bool DoCalc(Tag, const Constant*, const Constant*, Constant&);
 };
 
 #endif // _CONSTANT_H_
