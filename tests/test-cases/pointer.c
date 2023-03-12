4a91f2e59eab1db602a51082d57496a595364493
AinsleySnow <AinsleySnow@outlook.com>
2023-03-12 23:25:57 +0800

    add tests for pointers

---
+++ tests/test-cases/pointer.c
@@ -0,0 +1,77 @@
+void t1()
+{
+    int* a = 0;
+    char* b = 0;
+    char**** c = 0;
+    char* const * const * d = 0;
+    char* const restrict * e;
+    restrict const char* g = 0;
+}
+
+void t2()
+{
+    int a = 20;
+    int* pa = &a;
+    *pa = 40;
+}
+
+void t3()
+{
+    long a = 123456789;
+    long* p = &a;
+    p += 2;
+    p -= 3;
+    p = p + 3;
+    p = p - 3;
+}
+
+void t4()
+{
+    long a = 123456789;
+    long* p = &a;
+    p[2] = 20;
+    p[3] = 40;
+    *(p + 30) = 20;
+}
+
+int func()
+{
+    return 0;
+}
+
+int func2(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int (*func3(int a, int b, int c))()
+{
+    int d = a + b + c;
+    return func;
+}
+
+int (*func4(int a, int b, int c))(int, int, int)
+{
+    int (*pfunc)(int, int, int) = func2;
+    pfunc(a, b, c);
+    return pfunc;
+}
+
+int (*func5(int a, int b, int c))(int a, int b, int c)
+{
+    int (*pfunc)(int, int, int) = func2;
+    return pfunc;
+}
+
+int (*(*func6())(int, int, int))(int, int, int)
+{
+    return func5;
+}
+
+int main()
+{
+    t1();
+    t2();
+    t3();
+    t4();
+}
