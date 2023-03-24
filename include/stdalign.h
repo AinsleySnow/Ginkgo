9b2629391eb6be32a7b2fb0f84e5076c02e1ffd7
AinsleySnow <AinsleySnow@outlook.com>
2023-03-24 17:43:35 +0800

    Add first two headers. (Since it's easy to implement them.)

---
+++ include/stdalign.h
@@ -0,0 +1,3 @@
+// As is in the C23 standard,
+// the header <stdalign.h> provides no content.
+// 7.15
---
+++ include/stdbool.h
@@ -0,0 +1,6 @@
+#ifndef __GINKGO_STDBOOL_H
+#define __GINKGO_STDBOOL_H
+
+#define __bool_true_false_are_defined 1
+
+#endif // __GINKGO_STDBOOL_H
