b15bda3bb83cbaa71ba631f92ce2ea1f7eee0e9e
AinsleySnow <AinsleySnow@outlook.com>
2023-04-06 14:45:14 +0800

    new tags... for enum and other things.

--- src/ast/Tag.h
+++ src/ast/Tag.h
@@ -3,17 +3,23 @@
 
 enum class Tag
 {
-    _char = 1, _short = 2, _int = 4, _long = 8, _longlong = 24,
-    _float = 32, _double = 64, _unsigned = 128, _signed = 256,
-    _bool = 512, _void = 1024,
+    _char = 1, _short = 2, _int = 4, _long = 8,
+    _longlong = 24, _float = 32, _double = 64,
+    _unsigned = 128, _signed = 256, _bool = 512,
+    _void = 1024, _enum = 2048, _struct = 4096,
+    _union = 8192, _typedef = 16384, _atomic = 32768,
 
-    customedtype = 2048, typedefedtype, enumtype, atomictype,
+    // _typedef and _atomic can be used in two
+    // different scenarios.
+    // first: in type_specifier meaning that
+    // the type is defined by typedef; 
+    // second: in storage specifier,
+    // just a qualifier of a variable
+    _extern, _static, _thread_local,
 
-    _typedef, _extern, _static, _thread_local,
-    
     _auto, _register, _complex, _imaginary,
     
-    _const, _restrict, _volatile, _atomic,
+    _const, _restrict, _volatile,
 
     _inline, _noreturn,
 
