3454635882a07e908872b4e45e8f353268352e95
AinsleySnow <AinsleySnow@outlook.com>
2023-04-17 22:59:52 +0800

    add char.c

---
+++ tests/lang/char.c
@@ -0,0 +1,23 @@
+int main()
+{
+    char a = '1';
+    char b = L'你';
+    int c = u'好';
+    int d = U'吗';
+
+    char e = '\'';
+    char f = '\"';
+    char g = '"';
+    char h = '\\';
+    char i = '\?';
+    char j = '\141';
+    char k = '\x61';
+
+    char l = '\a';
+    char m = '\b';
+    char n = '\f';
+    char o = '\n';
+    char p = '\r';
+    char q = '\t';
+    char r = '\v';
+}
