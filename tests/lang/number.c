ffdd9cdbcc7a5aa6fd8bab1757be7ca8bbe586d7
AinsleySnow <AinsleySnow@outlook.com>
2023-04-18 17:30:32 +0800

    add number.c

---
+++ tests/lang/number.c
@@ -0,0 +1,33 @@
+int a = 1;
+int b = 01;
+int c = 010; // 8
+int d = 0x1;
+int e = 0x10; // 16
+int f = 0xABCDEF; // 11259375
+int g = 0xabcdef; // 11259375
+int h = 0b1;
+int i = 0b10; // 2
+
+int j = 12'34'56;
+int k = 012'34'56; // 42798
+int l = 0x12'34'56; // 1193046
+int m = 0b10'10'10; // 42
+
+int n = 0l;
+int o = 0L;
+int p = 0ul;
+int q = 0UL;
+int r = 0ll;
+int s = 0LL;
+int t = 0ull;
+int u = 0ULL;
+
+float v = 1.0f;
+float w = 1.0F;
+double x = 1.0l;
+double y = 1.0L;
+
+float z = 0x1.0p1;
+float aa = 0xABCDEF.ABCDEFP+4;
+float ab = 1.0e-1;
+float ac = 1.0e+1;
