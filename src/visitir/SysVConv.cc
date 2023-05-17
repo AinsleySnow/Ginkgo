cdf2d55e9f8514b81c93d321d116f84f2b6852ad
AinsleySnow <AinsleySnow@outlook.com>
2023-05-17 21:39:35 +0800

    implementation of System V calling convention

---
+++ src/visitir/SysVConv.cc
@@ -0,0 +1,107 @@
+#include "SysVConv.h"
+#include "IR/IROperand.h"
+#include "IR/IRType.h"
+
+
+void SysVConv::CheckParamClass(const IRType* t)
+{
+    if (t->IsInt())
+        wordclass_.push_back(ParamClass::integer);
+    else if (t->IsFloat() && t->Size() < 16)
+        wordclass_.push_back(ParamClass::sse);
+    else if (t->IsFloat())
+    {
+        wordclass_.push_back(ParamClass::x87);
+        wordclass_.push_back(ParamClass::x87up);
+    }
+
+    // for sort of things like _float128, _Decimal128...
+    // Well, I don't care about them, at least for now
+
+    // We're done if t is not aggregate type
+    // array is treated as a pointer
+    if (t->IsArray())
+        wordclass_.push_back(ParamClass::integer);
+}
+
+void SysVConv::Emplace(int i, const IRType* p, RegTag t)
+{
+    if (t == RegTag::none) return;
+    argvs_[i] = std::make_unique<x64Reg>(t, p->Size());
+}
+
+RegTag SysVConv::GetIntReg()
+{
+    switch (intcnt_++)
+    {
+    case 1: return RegTag::rdi;
+    case 2: return RegTag::rsi;
+    case 3: return RegTag::rdx;
+    case 4: return RegTag::rcx;
+    case 5: return RegTag::r8;
+    case 6: return RegTag::r9;
+    default: return RegTag::none;
+    }
+}
+
+RegTag SysVConv::GetVecReg()
+{
+    switch (veccnt_++)
+    {
+    case 0: return RegTag::xmm0;
+    case 1: return RegTag::xmm1;
+    case 2: return RegTag::xmm2;
+    case 3: return RegTag::xmm3;
+    case 4: return RegTag::xmm4;
+    case 5: return RegTag::xmm5;
+    case 6: return RegTag::xmm6;
+    case 7: return RegTag::xmm7;
+    default: return RegTag::none;
+    }
+}
+
+
+SysVConv::SysVConv(const FuncType* f)
+{
+    auto size = f->ParamType().size();
+    for (int i = 0; i < size; ++i)
+        CheckParamClass(f->ParamType()[i]);
+
+    // index for wordclass_
+    int windex = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        switch (wordclass_[windex])
+        {
+        case ParamClass::integer:
+            Emplace(i, f->ParamType()[i], GetIntReg());
+            windex++;
+            break;
+        case ParamClass::sse:
+            Emplace(i, f->ParamType()[i], GetVecReg());
+            windex++;
+            break;
+
+        // no need to implement this case - we
+        // don't even have __m256 or __m512.
+        // case ParamClass::sseup: break;
+
+        // If the class is X87, X87UP or COMPLEX_X87,
+        // it is passed in memory.
+        case ParamClass::x87:
+            windex += 2; // since x87up always follows behind x87
+            continue;
+        case ParamClass::memory:
+            windex++;
+            continue;
+        }
+    }
+}
+
+
+const x64* SysVConv::PlaceOfArgv(int index)
+{
+    if (argvs_.find(index) != argvs_.end())
+        return argvs_[index].get();
+    return nullptr;
+}
---
+++ src/visitir/SysVConv.h
@@ -0,0 +1,46 @@
+#ifndef _SYSV_CONV_H_
+#define _SYSV_CONV_H_
+
+#include "visitir/x64.h"
+#include <map>
+#include <memory>
+#include <vector>
+
+class IROperand;
+class FuncType;
+
+
+class SysVConv
+{
+public:
+    // complex_x87 is commented since Ginkgo
+    // doesn't support complex number yet
+    enum class ParamClass
+    {
+        integer, sse, sseup, x87, x87up,
+        /*complex_x87,*/ no_class, memory 
+    };
+
+    SysVConv(const FuncType*);
+    const x64* PlaceOfArgv(int);
+
+private:
+    void CheckParamClass(const IRType*);
+    void Emplace(int, const IRType*, RegTag);
+    RegTag GetIntReg();
+    RegTag GetVecReg();
+
+    // number of used integer and vector registers
+    int intcnt_{};
+    int veccnt_{};
+
+    // class of every eight bytes
+    std::vector<ParamClass> wordclass_{};
+
+    // to where does the xth parameter map?
+    // no map will be created if some parameter
+    // is to be placed into somewhere in the memory
+    std::map<int, std::unique_ptr<x64>> argvs_{};
+};
+
+#endif // _SYSV_CONV_H_
