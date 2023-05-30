688302d538db60851826691f5bbd452fe7e407ce
AinsleySnow <AinsleySnow@outlook.com>
2023-05-30 21:15:41 +0800

    tests for alignof, alignas and sizeof

---
+++ tests/lang/align.c
@@ -0,0 +1,57 @@
+#include "tests/test.h"
+
+void common()
+{
+    assert(alignof(char) == 1);
+    assert(alignof(short) == 2);
+    assert(alignof(int) == 4);
+    assert(alignof(long) == 8);
+
+    assert(alignof(float) == 4);
+    assert(alignof(double) == 8);
+    // assert(alignof(long double) == 16);
+
+    assert(_Alignof(char) == 1);
+    assert(_Alignof(short) == 2);
+    assert(_Alignof(int) == 4);
+    assert(_Alignof(long) == 8);
+
+    assert(_Alignof(float) == 4);
+    assert(_Alignof(double) == 8);
+    // assert(_Alignof(long double) == 16);
+}
+
+void array()
+{
+    // When applied to an array type, the result is
+    // the alignment requirement of the element type.
+    // (6.5.3.4[3])
+    assert(alignof(int [5]) == 4);
+    assert(alignof(int* [5]) == 8);
+}
+
+void as()
+{
+    int alignas(8) a = 10;
+    assert(alignof(a) == 8);
+
+    int alignas(double) b = 30;
+    assert(alignof(b) == 8);
+
+    // When multiple alignment specifiers occur in a
+    // declaration, the effective alignment requirement
+    // is the strictest specified alignment.
+    // (6.7.5[7])
+    int alignas(4) alignas(8) alignas(32) c = 40;
+    assert(alignof(c) == 32);
+}
+
+int main()
+{
+    common();
+    array();
+    as();
+
+    printf("OK\n");
+    return 0;
+}
---
+++ tests/lang/size.c
@@ -0,0 +1,59 @@
+#include "tests/test.h"
+
+void common()
+{
+    assert(sizeof(char) == 1);
+    assert(sizeof(short) == 2);
+    assert(sizeof(int) == 4);
+    assert(sizeof(long) == 8);
+
+    assert(sizeof(float) == 4);
+    assert(sizeof(double) == 8);
+    // assert(sizeof(long double) == 16);
+}
+
+void array()
+{
+    assert(sizeof(int [5]) == 20);
+    assert(sizeof(int* [5]) == 40);
+}
+
+void pointer()
+{
+    assert(sizeof(int*) == 8);
+    assert(sizeof(int (*)[5]) == 8);
+    assert(sizeof(int (*)(int)) == 8);
+    assert(sizeof(int (*(*) [5])(int)) == 8);
+}
+
+void expr()
+{
+    assert(sizeof(1 + 1) == 4);
+
+    int a = 10;
+    long b = 20;
+    assert(sizeof(a + b) == 8);
+
+    float c = 2.5;
+    assert(sizeof(b * c) == 4);
+}
+
+void var()
+{
+    int a = 20;
+    assert(sizeof(a) == 4);
+    int b[30];
+    assert(sizeof(b) == 120);
+}
+
+int main(void)
+{
+    common();
+    array();
+    pointer();
+    expr();
+    var();
+
+    printf("OK\n");
+    return 0;
+}
