f3f63571925fa290241816a3d48396c428d5522e
AinsleySnow <AinsleySnow@outlook.com>
2023-05-30 23:06:18 +0800

    rename qual-spec to qualify, and add new tests

--- tests/lang/qual-spec.c
+++
@@ -1,7 +0,0 @@
-const int a;
-const const const int b;
-volatile volatile const int c;
-volatile const volatile int d;
-volatile int e;
-volatile _Atomic int f;
-const _Atomic int g;
---
+++ tests/lang/qualify.c
@@ -0,0 +1,37 @@
+#include "tests/test.h"
+
+void decl()
+{
+    const int a;
+    const const const int b;
+    volatile volatile const int c;
+    volatile const volatile int d;
+    volatile int e;
+}
+
+void _volatile()
+{
+    int volatile a = 10;
+    a *= 10;
+    assert(a == 100);
+}
+
+int _restrict(int* restrict a, int* restrict b)
+{
+    *a = 5;
+    *b = 6;
+    return (*a) * (*b);
+}
+
+int main()
+{
+    decl();
+    _volatile();
+
+    int a = 10;
+    int b = 20;
+    int c = _restrict(&a, &b);
+    assert(*a == 5);
+    assert(*b == 6);
+    assert(c == 30);
+}
