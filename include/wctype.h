807b12db43917e4721c39fe073d4097ace4cad31
AinsleySnow <AinsleySnow@outlook.com>
2023-06-06 20:56:20 +0800

    add wctype.h

---
+++ include/wctype.h
@@ -0,0 +1,31 @@
+#ifndef __GINKGO_WCTYPE_H
+#define __GINKGO_WCTYPE_H
+
+#define __STDC_VERSION_WCHAR_H__ 202311L
+
+#include <wchar.h>
+
+typedef unsigned long int wctype_t;
+typedef const int* wctrans_t;
+
+int iswalnum(wint_t wc);
+int iswalpha(wint_t wc);
+int iswblank(wint_t wc);
+int iswcntrl(wint_t wc);
+int iswdigit(wint_t wc);
+int iswgraph(wint_t wc);
+int iswlower(wint_t wc);
+int iswprint(wint_t wc);
+int iswpunct(wint_t wc);
+int iswspace(wint_t wc);
+int iswupper(wint_t wc);
+int iswxdigit(wint_t wc);
+int iswctype(wint_t wc, wctype_t desc);
+
+wctype_t wctype(const char *property);
+wint_t towlower(wint_t wc);
+wint_t towupper(wint_t wc);
+wint_t towctrans(wint_t wc, wctrans_t desc);
+wctrans_t wctrans(const char *property);
+
+#endif // __GINKGO_WCTYPE_H
