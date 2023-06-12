4c60c25bf173c2033a27ff11285b5123ee0ab3d5
AinsleySnow <AinsleySnow@outlook.com>
2023-06-12 10:45:22 +0800

    add fuctions for accessing saved registers

--- include/stdarg.h
+++ include/stdarg.h
@@ -3,7 +3,8 @@
 
 #define __STDC_VERSION_STDARG_H__ 202311L
 
-typedef struct {
+typedef struct
+{
     unsigned int gp_offset;
     unsigned int fp_offset;
     void *overflow_arg_area;
@@ -11,6 +12,36 @@ typedef struct {
 } __Ginkgo_va_elem;
 
 typedef __Ginkgo_va_elem va_list[1];
+
+
+static void *__va_arg_mem(__va_elem *ap, int sz, int align)
+{
+    void *p = ap->overflow_arg_area;
+    if (align > 8)
+        p = (p + 15) / 16 * 16;
+    ap->overflow_arg_area = ((unsigned long)p + sz + 7) / 8 * 8;
+    return p;
+}
+
+static void *__va_arg_gp(__va_elem *ap, int sz, int align)
+{
+    if (ap->gp_offset >= 48)
+        return __va_arg_mem(ap, sz, align);
+
+    void *r = ap->reg_save_area + ap->gp_offset;
+    ap->gp_offset += 8;
+    return r;
+}
+
+static void *__va_arg_fp(__va_elem *ap, int sz, int align)
+{
+    if (ap->fp_offset >= 112)
+        return __va_arg_mem(ap, sz, align);
+
+    void *r = ap->reg_save_area + ap->fp_offset;
+    ap->fp_offset += 8;
+    return r;
+}
 
 
 #define va_arg(ap, ty) __Ginkgo_va_arg(ap, ty)
