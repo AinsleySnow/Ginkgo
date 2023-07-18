0467db3bf0fe3fccbb0eb93bd815cebff8b27ec7
AinsleySnow <AinsleySnow@outlook.com>
2023-07-19 00:17:05 +0800

    Add AddPass method for pass registering.

--- src/pass/Pipeline.h
+++ src/pass/Pipeline.h
@@ -2,22 +2,39 @@
 #define _PIPELINE_H_
 
 #include "pass/Pass.h"
-#include <vector>
+#include <cassert>
+#include <map>
+#include <memory>
+
+class Module;
 
 
 class Pipeline
 {
 public:
-    Pipeline(std::vector<Pass*> p) : passes_(std::move(p)) {}
+    Pipeline(Module* m) : module_(m) {}
+
+    template <class PASS, typename... IPASS,
+    typename = std::enable_if_t<std::conjunction_v<std::is_integral<IPASS>...>>
+    >
+    void AddPass(int i, IPASS&&... dep)
+    {
+        auto index2pass = [] (auto&& i) {
+            assert(passes_.count(i));
+            return passes_.at(i).get();
+        };
+        passes_[i] = std::make_unique<PASS>(m, index2pass(std::forward<IPASS>(dep))...);
+    }
 
     void ExecuteAll()
     {
         for (auto& pass : passes_)
-            pass->Execute();
+            pass.second->Execute();
     }
 
 private:
-    std::vector<Pass*> passes_{};
+    Module* module_{};
+    std::map<int, std::unique_ptr<Pass>> passes_{};
 };
 
 #endif // _PIPELINE_H_
