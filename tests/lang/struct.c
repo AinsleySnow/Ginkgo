dddfec829e7580d0f29bdd07a7b88cd311ac4f1e
AinsleySnow <AinsleySnow@outlook.com>
2023-08-14 23:54:09 +0800

    Correctly translate struct.c to IR.

--- tests/lang/struct.c
+++ tests/lang/struct.c
@@ -1,4 +1,4 @@
-#include "tests/test.h"
+#include "test.h"
 
 struct s
 {
@@ -53,6 +53,14 @@ void arrow_expr()
     assert(d == 25);
 }
 
+void dereference()
+{
+    struct s ss;
+    struct s* p = &ss;
+    (*p).a = 20;
+    assert(ss.a == 20);
+}
+
 struct t
 {
     char a;
@@ -79,8 +87,8 @@ void padding()
 
     assert(sizeof(struct t) == 16);
     assert(pb - pa == 2);
-    assert(pc - pb == 4);
-    assert(pd - pc == 8);
+    assert(pc - pb == 2);
+    assert(pd - pc == 4);
 
     struct u vu;
     pa = (long)&vu.a;
@@ -92,6 +100,23 @@ void padding()
     assert(pb - pa == 8);
     assert(pc - pb == 4);
     assert(pd - pc == 2);
+}
+
+void array()
+{
+    struct u array[20];
+    array[0].a = 20;
+    array[0].b = 40;
+    array[1].c = 30;
+    array[10].c = 50;
+
+    assert(array[0].a == 20);
+    assert(array[0].b == 40);
+    assert(array[1].c == 30);
+    assert(array[10].c == 50);
+
+    int* pu = array;
+    assert(pu[3] == 40);
 }
 
 void size()
@@ -106,12 +131,12 @@ void size()
     assert(sizeof(vw) == 12);
 }
 
-void cast()
+void nest()
 {
     struct s1
     {
-        struct s2 { int a, int b } f1;
-        struct s3 { int c, int d } f2;
+        struct s2 { int a; int b; } f1;
+        struct s3 { int c; int d; } f2;
     } s;
 
     s.f1.a = 10;
@@ -163,6 +188,57 @@ void anonymous()
     assert(vw.w.l == 8);
 }
 
+void instruct()
+{
+    struct a
+    {
+        struct b { int c; int d; };
+        struct b e;
+    };
+
+    struct a g;
+    g.e.c = 20;
+    g.e.d = 30;
+    assert(g.e.c == 20);
+    assert(g.e.d == 30);
+}
+
+void dup()
+{
+    struct d
+    {
+        struct
+        {
+            int i;
+        } dp;
+        int i;
+    };
+
+    struct d vd;
+    vd.dp.i = 20;
+    vd.i = 10;
+    assert(vd.dp.i == 20);
+    assert(vd.i == 10);
+}
+
+struct ret
+{
+    int a;
+    int b;
+    int c;
+    int d;
+};
+
+struct ret retstruct()
+{
+    struct ret r;
+    r.a = 20;
+    r.b = 30;
+    r.c = 40;
+    r.d = 50;
+    return r;
+}
+
 int main()
 {
     dot_assign();
@@ -171,9 +247,16 @@ int main()
     arrow_expr();
     padding();
     size();
+    nest();
     unnamed();
     anonymous();
+    dup();
 
-    printf("OK\n");
-    return 0;
+    struct ret r = retstruct();
+    assert(r.a == 20);
+    assert(r.b == 30);
+    assert(r.c == 40);
+    assert(r.d == 50);
+
+    SUCCESS;
 }
